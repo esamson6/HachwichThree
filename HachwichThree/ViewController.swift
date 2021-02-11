cbe881835f4787db69d1d525d58e10ad517cea0f
esamson6 <77295762+esamson6@users.noreply.github.com>
2021-02-11 09:39:54 -1000

    Completed Part 4 - Problem sets

--- HachwichThree/ViewController.swift
+++ HachwichThree/ViewController.swift
@@ -9,10 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBOutlet weak var yourLabel: UILabel!
+    
     //global variable are visible to entire program
     
     var firstString = "The background color will turn blue"
     var secondString = "The background color will turn green"
+    
+    //two new variables for problem set 2
+    
+    @IBOutlet weak var thirdLabel: UILabel!
+    var firstInte = 7
+    var secondInte = 8
     
 
     override func viewDidLoad() {
@@ -42,9 +52,19 @@ class ViewController: UIViewController {
             
         }
         
+        //Problem set #1
+    }
+    
+    
+    @IBAction func magicButtonPressed(_ sender: Any)
+    {
+        if firstInte < secondInte
         
-        
-        
+        {
+            self.view.backgroundColor = UIColor.green
+            thirdLabel.text = "Green"
+            print("I completed both problem sets")
+        }
     }
     
 }
